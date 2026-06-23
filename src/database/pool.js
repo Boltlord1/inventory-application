f64e8ec465044e0bc9a70bf1001c6a4f0c5d3995
Boltlord1 <boltlord1@gmail.com>
2026-06-23 07:33:54 +0500

    add support for vercel variables

--- src/database/pool.js
+++ src/database/pool.js
@@ -1,4 +1,16 @@
 import { Pool } from 'pg'
 
-const url = process.env.DATABASE_URL
-export default new Pool({ connectionString: url })
+let connectionString = process.env.POSTGRES_URL
+if (!connectionString) {
+	console.error('No database connection string found')
+} else {
+    const url = new URL(connectionString)
+    url.searchParams.delete('sslmode')
+    connectionString = url.toString()
+}
+
+export default new Pool({
+    connectionString,
+    ssl: { rejectUnauthorized: false },
+	max: 1
+})
